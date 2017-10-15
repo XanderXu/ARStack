1dbe13014b2c892d5155ad03d149903e7838ab6f
Xander Xu <xander@texchi.com>
2017-10-15 12:43:29 +0800

    prepare step2

--- step2-High-Rise-Final/High Rise/ViewController.swift
+++ step2-High-Rise-Final/High Rise/ViewController.swift
@@ -24,6 +24,10 @@ import UIKit
 import SceneKit
 import SpriteKit
 
+let boxheight:CGFloat = 0.05
+let boxLengthWidth:CGFloat = 0.4
+let actionOffet:Float = 0.6
+let actionSpeed:Float = 0.011
 class ViewController: UIViewController {
   @IBOutlet weak var scnView: SCNView!
   @IBOutlet weak var playButton: UIButton!
@@ -35,11 +39,10 @@ class ViewController: UIViewController {
   var height = 0
   
   
-  var previousSize = SCNVector3(1, 0.2, 1)
-  var previousPosition = SCNVector3(0, 0.1, 0)
-  var currentSize = SCNVector3(1, 0.2, 1)
+  var previousSize = SCNVector3(boxLengthWidth, boxheight, boxLengthWidth)
+  var previousPosition = SCNVector3(0, boxheight*0.5, 0)
+  var currentSize = SCNVector3(boxLengthWidth, boxheight, boxLengthWidth)
   var currentPosition = SCNVector3Zero
-  
   
   var offset = SCNVector3Zero
   var absoluteOffset = SCNVector3Zero
@@ -54,7 +57,7 @@ class ViewController: UIViewController {
   
     scnScene = SCNScene(named: "HighRise.scnassets/Scenes/GameScene.scn")
     scnView.scene = scnScene
-    
+    scnView.allowsCameraControl = true
     scnView.isPlaying = true
     scnView.delegate = self
     
@@ -83,8 +86,7 @@ class ViewController: UIViewController {
     let gameScene = SCNScene(named: "HighRise.scnassets/Scenes/GameScene.scn")!
     let transition = SKTransition.fade(withDuration: 1.0)
     scnScene = gameScene
-    let mainCamera = scnScene.rootNode.childNode(withName: "Main Camera", recursively: false)!
-    scnView.present(scnScene, with: transition, incomingPointOfView: mainCamera, completionHandler: nil)
+    scnView.present(gameScene, with: transition, incomingPointOfView: nil, completionHandler: nil)
     
     height = 0
     scoreLabel.text = "\(height)"
@@ -92,35 +94,24 @@ class ViewController: UIViewController {
     direction = true
     perfectMatches = 0
     
-    previousSize = SCNVector3(1, 0.2, 1)
-    previousPosition = SCNVector3(0, 0.1, 0)
     
-    currentSize = SCNVector3(1, 0.2, 1)
-    currentPosition = SCNVector3Zero
-    
-    let boxNode = SCNNode(geometry: SCNBox(width: 1, height: 0.2, length: 1, chamferRadius: 0))
-    boxNode.position.z = -1.25
-    boxNode.position.y = 0.1
+    let boxNode = SCNNode(geometry: SCNBox(width: boxLengthWidth, height: boxheight, length: boxLengthWidth, chamferRadius: 0))
+    boxNode.position.z = -actionOffet
+    boxNode.position.y = Float(boxheight * 0.5)
     boxNode.name = "Block\(height)"
-    boxNode.geometry?.firstMaterial?.diffuse.contents = UIColor(colorLiteralRed: 0.01 * Float(height), green: 0, blue: 1, alpha: 1)
+    boxNode.geometry?.firstMaterial?.diffuse.contents = UIColor(red: 0.1 * CGFloat(height % 10), green: 0.3*CGFloat(height%30), blue: 1-0.1 * CGFloat(height % 10), alpha: 1)
     scnScene.rootNode.addChildNode(boxNode)
   }
   
   
   func gameOver() {
-    let mainCamera = scnScene.rootNode.childNode(withName: "Main Camera", recursively: false)!
     
     let fullAction = SCNAction.customAction(duration: 0.3) { _,_ in
-      let moveAction = SCNAction.move(to: SCNVector3Make(mainCamera.position.x, mainCamera.position.y * (3/4), mainCamera.position.z), duration: 0.3)
-      mainCamera.runAction(moveAction)
-      if self.height <= 15 {
-        mainCamera.camera?.orthographicScale = 1
-      } else {
-        mainCamera.camera?.orthographicScale = Double(Float(self.height/2) / mainCamera.position.y)
-      }
+      let moveAction = SCNAction.move(to: SCNVector3Make(0, 0, 0), duration: 0.3)
+      self.scnScene.rootNode.runAction(moveAction)
     }
     
-    mainCamera.runAction(fullAction)
+    scnScene.rootNode.runAction(fullAction)
     playButton.isHidden = false
   }
 
@@ -154,38 +145,38 @@ class ViewController: UIViewController {
       
       checkPerfectMatch(currentBoxNode)
 
-      currentBoxNode.geometry = SCNBox(width: CGFloat(newSize.x), height: 0.2, length: CGFloat(newSize.z), chamferRadius: 0)
+      currentBoxNode.geometry = SCNBox(width: CGFloat(newSize.x), height: boxheight, length: CGFloat(newSize.z), chamferRadius: 0)
       currentBoxNode.position = SCNVector3Make(currentPosition.x + (offset.x/2), currentPosition.y, currentPosition.z + (offset.z/2))
       currentBoxNode.physicsBody = SCNPhysicsBody(type: .static, shape: SCNPhysicsShape(geometry: currentBoxNode.geometry!, options: nil))
-      
+      currentBoxNode.geometry?.firstMaterial?.diffuse.contents = UIColor(red: 0.1 * CGFloat(height % 10), green: 0.3*CGFloat(height%30), blue: 1-0.1 * CGFloat(height % 10), alpha: 1)
       addBrokenBlock(currentBoxNode)
       addNewBlock(currentBoxNode)
       playSound(sound: "SliceBlock", node: currentBoxNode)
 
       if height >= 5 {
-        let moveUpAction = SCNAction.move(by: SCNVector3Make(0.0, 0.2, 0.0), duration: 0.2)
-        let mainCamera = scnScene.rootNode.childNode(withName: "Main Camera", recursively: false)!
-        mainCamera.runAction(moveUpAction)
+        let moveUpAction = SCNAction.move(by: SCNVector3Make(0.0, Float(-boxheight), 0.0), duration: 0.2)
+        
+        scnScene.rootNode.runAction(moveUpAction)
       }
       
       scoreLabel.text = "\(height+1)"
       
-      previousSize = SCNVector3Make(newSize.x, 0.2, newSize.z)
+      previousSize = SCNVector3Make(newSize.x, Float(boxheight), newSize.z)
       previousPosition = currentBoxNode.position
       height += 1
     }
   }
   
   func addNewBlock(_ currentBoxNode: SCNNode) {
-    let newBoxNode = SCNNode(geometry: currentBoxNode.geometry)
-    newBoxNode.position = SCNVector3Make(currentBoxNode.position.x, currentPosition.y + 0.2, currentBoxNode.position.z)
+    let newBoxNode = SCNNode(geometry: SCNBox(width: CGFloat(newSize.x), height: boxheight, length: CGFloat(newSize.z), chamferRadius: 0))
+    newBoxNode.position = SCNVector3Make(currentBoxNode.position.x, currentPosition.y + Float(boxheight), currentBoxNode.position.z)
     newBoxNode.name = "Block\(height+1)"
-    newBoxNode.geometry?.firstMaterial?.diffuse.contents = UIColor(colorLiteralRed: 0.01 * Float(height), green: 0, blue: 1, alpha: 1)
+    newBoxNode.geometry?.firstMaterial?.diffuse.contents = UIColor(red: 0.1 * CGFloat((height+1) % 10), green: 0.3*CGFloat((height+1)%30), blue: 1-0.1 * CGFloat((height+1) % 10), alpha: 1)
     
     if height % 2 == 0 {
-      newBoxNode.position.x = -1.25
+      newBoxNode.position.x = -actionOffet
     } else {
-      newBoxNode.position.z = -1.25
+      newBoxNode.position.z = -actionOffet
     }
     
     scnScene.rootNode.addChildNode(newBoxNode)
@@ -197,7 +188,7 @@ class ViewController: UIViewController {
     
     if height % 2 == 0 && absoluteOffset.z > 0 {
       // 1
-      brokenBoxNode.geometry = SCNBox(width: CGFloat(currentSize.x), height: 0.2, length: CGFloat(absoluteOffset.z), chamferRadius: 0)
+      brokenBoxNode.geometry = SCNBox(width: CGFloat(currentSize.x), height: boxheight, length: CGFloat(absoluteOffset.z), chamferRadius: 0)
       
       // 2
       if offset.z > 0 {
@@ -210,12 +201,12 @@ class ViewController: UIViewController {
       
       // 3
       brokenBoxNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(geometry: brokenBoxNode.geometry!, options: nil))
-      brokenBoxNode.geometry?.firstMaterial?.diffuse.contents = UIColor(colorLiteralRed: 0.01 * Float(height), green: 0, blue: 1, alpha: 1)
+      brokenBoxNode.geometry?.firstMaterial?.diffuse.contents = UIColor(red: 0.1 * CGFloat(height % 10), green: 0.3*CGFloat(height%30), blue: 1-0.1 * CGFloat(height % 10), alpha: 1)
       scnScene.rootNode.addChildNode(brokenBoxNode)
       
       // 4
     } else if height % 2 != 0 && absoluteOffset.x > 0 {
-      brokenBoxNode.geometry = SCNBox(width: CGFloat(absoluteOffset.x), height: 0.2, length: CGFloat(currentSize.z), chamferRadius: 0)
+      brokenBoxNode.geometry = SCNBox(width: CGFloat(absoluteOffset.x), height: boxheight, length: CGFloat(currentSize.z), chamferRadius: 0)
       
       if offset.x > 0 {
         brokenBoxNode.position.x = currentBoxNode.position.x - (offset.x/2) - ((currentSize - offset).x/2)
@@ -226,31 +217,31 @@ class ViewController: UIViewController {
       brokenBoxNode.position.z = currentBoxNode.position.z
       
       brokenBoxNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(geometry: brokenBoxNode.geometry!, options: nil))
-      brokenBoxNode.geometry?.firstMaterial?.diffuse.contents = UIColor(colorLiteralRed: 0.01 * Float(height), green: 0, blue: 1, alpha: 1)
+      brokenBoxNode.geometry?.firstMaterial?.diffuse.contents = UIColor(red: 0.1 * CGFloat(height % 10), green: 0.3*CGFloat(height%30), blue: 1-0.1 * CGFloat(height % 10), alpha: 1)
       scnScene.rootNode.addChildNode(brokenBoxNode)
     }
   }
 
   func checkPerfectMatch(_ currentBoxNode: SCNNode) {
-    if height % 2 == 0 && absoluteOffset.z <= 0.03 {
+    if height % 2 == 0 && absoluteOffset.z <= 0.005 {
       playSound(sound: "PerfectFit", node: currentBoxNode)
       currentBoxNode.position.z = previousPosition.z
       currentPosition.z = previousPosition.z
       perfectMatches += 1
       if perfectMatches >= 7 && currentSize.z < 1 {
-        newSize.z += 0.05
+        newSize.z += 0.005
       }
       
       offset = previousPosition - currentPosition
       absoluteOffset = offset.absoluteValue()
       newSize = currentSize - absoluteOffset
-    } else if height % 2 != 0 && absoluteOffset.x <= 0.03 {
+    } else if height % 2 != 0 && absoluteOffset.x <= 0.005 {
       playSound(sound: "PerfectFit", node: currentBoxNode)
       currentBoxNode.position.x = previousPosition.x
       currentPosition.x = previousPosition.x
       perfectMatches += 1
       if perfectMatches >= 7 && currentSize.x < 1 {
-        newSize.x += 0.05
+        newSize.x += 0.005
       }
       
       offset = previousPosition - currentPosition
@@ -271,7 +262,7 @@ extension ViewController: SCNSceneRendererDelegate {
   func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
     
     for node in scnScene.rootNode.childNodes {
-      if node.presentation.position.y <= -20 {
+      if node.presentation.position.y <= -10 {
         node.removeFromParentNode()
       }
     }
@@ -281,32 +272,32 @@ extension ViewController: SCNSceneRendererDelegate {
       // 2
       if height % 2 == 0 {
         // 3
-        if currentNode.position.z >= 1.25 {
+        if currentNode.position.z >= actionOffet {
           direction = false
-        } else if currentNode.position.z <= -1.25 {
+        } else if currentNode.position.z <= -actionOffet {
           direction = true
         }
         
         // 4
         switch direction {
         case true:
-          currentNode.position.z += 0.03
+          currentNode.position.z += actionSpeed
         case false:
-          currentNode.position.z -= 0.03
+          currentNode.position.z -= actionSpeed
         }
         // 5
       } else {
-        if currentNode.position.x >= 1.25 {
+        if currentNode.position.x >= actionOffet {
           direction = false
-        } else if currentNode.position.x <= -1.25 {
+        } else if currentNode.position.x <= -actionOffet {
           direction = true
         }
         
         switch direction {
         case true:
-          currentNode.position.x += 0.03
+          currentNode.position.x += actionSpeed
         case false:
-          currentNode.position.x -= 0.03
+          currentNode.position.x -= actionSpeed
         }
       }
     }
